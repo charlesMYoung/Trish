1f5aa542d3bf2ecc3e8dc911ad530b4b987a434b
Arferik <610231551@qq.com>
2023-11-30 00:40:07 +0800

    feat: 新增侧边栏

---
+++ src/components/sidebar/sidebar-menu.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export function SidebarMenu({ children }: { children: React.ReactNode }) {
+  return <div className="text-sm ">{children}</div>;
+}
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,49 +3,58 @@ import {
   Accordion,
   AccordionItem,
   Listbox,
-  ListboxSection,
   ListboxItem,
 } from "@nextui-org/react";
 import { SideBarConfig } from "@/config/appConfig";
+import { SidebarItem } from "./sidebar-item";
+import { SidebarMenu } from "./sidebar-menu";
 export function SideBar() {
   return (
-    <div className="flex w-60 h-screen flex-col bg-default-100">
-      <Accordion
-        showDivider={false}
-        className="p-2 flex flex-col gap-1 w-full max-w-[300px] h-screen overflow-y-auto"
-        variant="shadow"
-        itemClasses={{
-          base: "py-0 w-full",
-          title: "font-normal text-medium",
-          trigger:
-            "px-2 py-0 data-[hover=true]:bg-default-100 rounded-lg h-14 flex items-center",
-          indicator: "text-medium",
-          content: "text-small px-2",
-        }}
-      >
-        {SideBarConfig.map((sidebar) => {
-          return (
+    <div className="flex w-72 flex-col box-border px-4 space-y-3">
+      {SideBarConfig.map((sidebar) => {
+        return sidebar.path ? (
+          <SidebarItem
+            key={sidebar.name}
+            href={sidebar.path}
+            icon={sidebar.icon}
+          >
+            {sidebar.name}
+          </SidebarItem>
+        ) : sidebar.children && sidebar.children.length > 0 ? (
+          <Accordion
+            title={sidebar.name}
+            itemClasses={{
+              base: "py-0 w-full",
+              title: "font-normal text-medium",
+              trigger:
+                "px-2 py-0 data-[hover=true]:bg-default-100 rounded-lg h-14 flex items-center",
+              indicator: "text-medium",
+              content: "text-small px-2 ",
+            }}
+          >
             <AccordionItem
-              itemID={sidebar.name}
               startContent={sidebar.icon}
+              itemID={sidebar.name}
               key={sidebar.name}
               aria-label={sidebar.name}
               hideIndicator={sidebar.children ? false : true}
               title={sidebar.name}
             >
-              {sidebar.children ? (
-                <Listbox>
-                  {sidebar.children.map((child) => {
+              {sidebar.children
+                ? sidebar.children.map((child) => {
                     return (
-                      <ListboxItem key={child.name}>{child.name}</ListboxItem>
+                      <SidebarItem key={child.name} href={child.path}>
+                        {child.name}
+                      </SidebarItem>
                     );
-                  })}
-                </Listbox>
-              ) : null}
+                  })
+                : null}
             </AccordionItem>
-          );
-        })}
-      </Accordion>
+          </Accordion>
+        ) : (
+          <SidebarMenu key={sidebar.name}>{sidebar.name}</SidebarMenu>
+        );
+      })}
     </div>
   );
 }
