4470f69ccb86d87aabc38957a3ca7fb6c10e5b70
Charles Maxwell Yang <128476908+charlesMYoung@users.noreply.github.com>
2023-12-05 14:59:17 +0000

    feat✨: 新增novel 编辑器

--- src/app/dashboard/article/write/page.tsx
+++ src/app/dashboard/article/write/page.tsx
@@ -1,3 +1,13 @@
+'use client'
+
+import { Editor } from '@tiptap/core'
+import { Editor as EditorView } from 'novel'
+
 export default function Write() {
-  return <>Write</>
+  const onUpdate = (edit?: Editor) => {
+    console.log(edit)
+  }
+
+  // Renders the editor instance using a React component.
+  return <EditorView className="bg-background" onUpdate={onUpdate} />
 }
