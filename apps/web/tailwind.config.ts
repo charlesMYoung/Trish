a0347abe90437c61fc010fd7bb75add2ccf481ee
Arferik <610231551@qq.com>
2024-02-23 00:10:33 +0800

    feat: 项目结构转换

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,12 @@
+import tailwindConfig from '@trish/tailwind-config'
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  presets: [tailwindConfig],
+} satisfies Config
