337acc7ba5e34cdc5a957459ca6e07388c4192fc
Arferik <610231551@qq.com>
2024-02-25 15:50:40 +0800

    build: web 去除drizzle配置

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+import { type Config } from 'drizzle-kit'
 
-import { env } from "~/env.js";
+import { env } from './src/env'
 
 export default {
-  schema: "./src/schema.ts",
-  driver: "pg",
+  schema: './src/schema.ts',
+  driver: 'pg',
   dbCredentials: {
     connectionString: env.DATABASE_URL,
   },
-  tablesFilter: ["trish_*"],
-} satisfies Config;
+  tablesFilter: ['trish_*'],
+} satisfies Config
